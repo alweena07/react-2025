494e2a3410818fcb553d34fb8bcc0f14ee66725e
alweena07 <alweena.iqbal@logistiex.com>
2025-01-09 15:14:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {Fragment } from "react";
+import React, { Fragment } from "react";
 //import Greet from "./props/Greet";
 //import Welcome from "./props/Welcome";
 //import Count from "./state/Count";
-//import ParentComponent from "./methods-as-props/ParentComponent";
+// import ParentComponent from "./methods-as-props/ParentComponent";
 //import UserGreeting from "./conditional-rendering/UserGreeting";
 //import ListData from "./list-rendering/ListData";
 import HookList from "./hooks/listHooks";
@@ -11,7 +11,11 @@ import HookList from "./hooks/listHooks";
 //import User from "./child-to-parent/User";
 //import MultiPleInput from "./hooks/MultipleInputHooks"
 //import PushArrayOnBtnClick from "./count/PushArrayOnBtnClick";
-//import Login from "./be-U/Login"
+import Login from "./be-U/Login";
+import LifecycleA from "./lifecycle-methods/LifecycleA";
+// import ParentComponent from "./Pure-Component/ParentComponent";
+import UserTable from "./user-table/user-table";
+import ParentComponent from "./Pure-Component/parent-component-functional";
 //import SocialLogin from "./social-login/GoogleLogin";
 //import Facebook from "./social-login/FacebookLogin";
 //import Menu from '../src/Menu/Menu';
@@ -20,38 +24,34 @@ import HookList from "./hooks/listHooks";
 //import Practice from '../src/Practice';
 //import FilterableProductTable from './component/mock/FilterableProductTable';
 //import Cookie from './cookie/Cookie';
+import ReactCrudOperations from "./crud/index";
+
 
 function App() {
-
- /*  const parentAlert = (d) => {
+  /*  const parentAlert = (d) => {
 
     alert(d.lastName)
 
 
   }  */
 
- /*  const handleLogin = val => {
-
+  const handleLogin = (val) => {
     let username = val.userName;
     let password = val.passWord;
     //console.log(val)
 
-    if(username === "abc" && password === 'abc'){
-      alert('Login Successful')
-
+    if (username === "abc" && password === "abc") {
+      alert("Login Successful");
+    } else {
+      alert(" Login Unsuccessful");
     }
-    else{
-      alert(' Login Unsuccessful')
-    }
-  } */
+  };
 
-    return <> 
+  return (
+    <>
+      {/*   <Welcome Tag = 'h1' Tag2 = 'h4' /> */}
 
-  {/*   <Welcome Tag = 'h1' Tag2 = 'h4' /> */}
-   
-
-
-   {/*   
+      {/*   
     <Greet name ="Alweena" heroName = "Diana">
     </Greet>
     <Greet name ="Anusha"  heroName = "John">
@@ -63,30 +63,34 @@ function App() {
     </Welcome>
    */}
 
-    { /*
+      {/*
       <Count />
         <ParentComponent/>
          <UserGreeting />
           
-    */ }
+    */}
 
-  {/*
+      {/*
        <div className="user-info">
           <SearchImage />
        </div> 
   */}
 
-  {/*  <User handleClick = {parentAlert} name = "Anusha"/>  */}
+      {/*  <User handleClick = {parentAlert} name = "Anusha"/>  */}
 
-  {/*  <ListData />  */}
+      {/*  <ListData />  */}
 
-  {/* <SocialLogin handleLogin = {handleLogin} /><br/>
+      {/* <SocialLogin handleLogin = {handleLogin} /><br/>
   <Facebook />
  */}
- <HookList/>
-  
+      {/*  <HookList/> */}
+      {/* <Login handleLogin={handleLogin} /> */}
+      {/* <LifecycleA /> */}
+      {/* <ParentComponent/> */}
+      {/* <UserTable /> */}
+      <ReactCrudOperations />
     </>
-  
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./styles/style.css"
-import App from './App';
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/style.css";
+import App from "./App";
 
-import reportWebVitals from './reportWebVitals';
-
-
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
